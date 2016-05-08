8c335e87d2fbcc2135d55a85929063d8b7190f01
L-Dogg <szymek94sul@gmail.com>
2016-05-08 11:54:22 +0200

    Add argument parsing, add grades dictionary

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import argparse
 import urllib.request
 
 from bs4 import BeautifulSoup
@@ -5,6 +6,15 @@ from bs4 import BeautifulSoup
 URL = "http://www.mini.pw.edu.pl/~dobrowolskip/lab/asd2lab/l16/results.html"
 USERNAME = 'student'
 PASSWORD = 'lab2asd'
+
+parser = argparse.ArgumentParser(description='ASD2 Lab Scores')
+parser.add_argument('name', metavar='name', nargs=1, help='Student name')
+parser.add_argument('surname', metavar='surname', nargs=1, help='Student surname')
+args = parser.parse_args()
+
+class bcolors:
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
 
 p = urllib.request.HTTPPasswordMgrWithDefaultRealm()
 p.add_password(None, URL, USERNAME, PASSWORD)
@@ -17,13 +27,16 @@ page = urllib.request.urlopen(URL).read()
 
 soup = BeautifulSoup(page, 'html.parser')
 table = soup.find_all('table')[0]
-student_name = u'Anonymous'
-with open('parsed', 'w') as f:
-    for row in table.find_all('tr')[1:]:
-        cells = row.find_all('td')
-        print(cells[0].text)
-        if(cells[0].text.strip() == student_name):
-            for cell in cells[1:]:
-                f.write(cell.text)
-            f.write('\n')
-f.close()
+student_name = '%s %s' % (args.name[0], args.surname[0])
+print(student_name)
+student_marks = {}
+i = 1
+for row in table.find_all('tr')[1:]:
+    cells = row.find_all('td')
+    if cells[0].text.strip() == student_name:
+        for cell in cells[1:]:
+            if not cell.text.strip() == '':
+                student_marks[i] = float(cell.text.strip())
+                i = i + 1
+
+print(student_marks)
