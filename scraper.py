040bf8ad60d496f13d97551905fe25191b4bd34b
L-Dogg <szymek94sul@gmail.com>
2016-05-08 12:01:44 +0200

    Rewrite into functions

--- scraper.py
+++ scraper.py
@@ -7,36 +7,47 @@ URL = "http://www.mini.pw.edu.pl/~dobrowolskip/lab/asd2lab/l16/results.html"
 USERNAME = 'student'
 PASSWORD = 'lab2asd'
 
-parser = argparse.ArgumentParser(description='ASD2 Lab Scores')
-parser.add_argument('name', metavar='name', nargs=1, help='Student name')
-parser.add_argument('surname', metavar='surname', nargs=1, help='Student surname')
-args = parser.parse_args()
-
 class bcolors:
     OKGREEN = '\033[92m'
     FAIL = '\033[91m'
 
-p = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-p.add_password(None, URL, USERNAME, PASSWORD)
+def parseArguments():
+    parser = argparse.ArgumentParser(description='ASD2 Lab Scores')
+    parser.add_argument('name', metavar='name', nargs=1, help='Student name')
+    parser.add_argument('surname', metavar='surname', nargs=1, help='Student surname')
+    return parser.parse_args()
 
-handler = urllib.request.HTTPBasicAuthHandler(p)
-opener = urllib.request.build_opener(handler)
-urllib.request.install_opener(opener)
+def getPage():
+    p = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    p.add_password(None, URL, USERNAME, PASSWORD)
 
-page = urllib.request.urlopen(URL).read()
+    handler = urllib.request.HTTPBasicAuthHandler(p)
+    opener = urllib.request.build_opener(handler)
+    urllib.request.install_opener(opener)
 
-soup = BeautifulSoup(page, 'html.parser')
-table = soup.find_all('table')[0]
-student_name = '%s %s' % (args.name[0], args.surname[0])
-print(student_name)
-student_marks = {}
-i = 1
-for row in table.find_all('tr')[1:]:
-    cells = row.find_all('td')
-    if cells[0].text.strip() == student_name:
-        for cell in cells[1:]:
-            if not cell.text.strip() == '':
-                student_marks[i] = float(cell.text.strip())
-                i = i + 1
+    return urllib.request.urlopen(URL).read()
 
-print(student_marks)
+def parsePage(page, args):
+    soup = BeautifulSoup(page, 'html.parser')
+    table = soup.find_all('table')[0]
+    student_name = '%s %s' % (args.name[0], args.surname[0])
+    print(student_name)
+    student_marks = {}
+    i = 1
+    for row in table.find_all('tr')[1:]:
+        cells = row.find_all('td')
+        if cells[0].text.strip() == student_name:
+            for cell in cells[1:]:
+                if not cell.text.strip() == '':
+                    student_marks[i] = float(cell.text.strip())
+                    i = i + 1
+    return student_marks
+
+def main():
+    args = parseArguments()
+    page = getPage()
+    student_marks = parsePage(page, args)
+    print(student_marks)
+
+if __name__ == '__main__':
+    main()
